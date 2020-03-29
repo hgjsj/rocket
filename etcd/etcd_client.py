c8e42d7fc14ef017d2bc329178d6b996fd79a0a4
Sutton Sun <fu-qi.sun@dxc.com>
2020-03-29 18:11:49 +0800

    update

--- etcd/etcd_client.py
+++ etcd/etcd_client.py
@@ -4,42 +4,8 @@ import datetime
 
 ETCD_WATCH_COUNT = 0
 
-def pait_log(char):
-    def print_line(func):
-        def printing(text):
-            print (char * 15)
-            func(text)
-            print (char * 15)
-        return printing
-    return print_line
-
-def print_line1(func):
-    def printing(text):
-        print ("*" * 15)
-        func(text)
-        print ("*" * 15)
-    return printing
-
-def print_line2(func):
-    def printing(text):
-        print ("~" * 15)
-        func(text)
-        print ("~" * 15)
-    return printing
-
-#@pait_log('=')
-@print_line1
-@print_line2
-def log(text):
-    print ("now is %s" % text)
-
-def log1(text):
-    print ("now is %s" % text)
 
 if __name__ == '__main__':
-    log(datetime.datetime.now())
-    a = print_line1(print_line2(log1))
-    a(datetime.datetime.now())
     etcd = etcd3.client(host='192.168.30.144', port=2379, ca_cert="etcd/ca.pem", cert_cert="etcd/client.pem",cert_key='etcd/client-key.pem')
 
     #with etcd.lock('/key') as locker:
